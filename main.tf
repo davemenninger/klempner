cf2a5ff32cfeef4f0112bea2f92c4effb5cd22a4
Dave Menninger <dave.menninger@online-rewards.com>
2020-03-04 14:25:30 -0500

    add ecs cluster and rds instance

--- main.tf
+++ main.tf
@@ -312,3 +312,170 @@ resource "aws_ecr_repository" "ecr_repository" {
 resource "aws_ecs_cluster" "foo" {
   name = "${var.appname}-cluster"
 }
+
+resource "aws_ecs_service" "web" {
+  name            = "web"
+  cluster         = "${aws_ecs_cluster.foo.id}"
+  # task_definition = "${aws_ecs_task_definition.web.arn}"
+  task_definition = "${aws_ecs_task_definition.web.family}:${max("${aws_ecs_task_definition.web.revision}", "${data.aws_ecs_task_definition.web.revision}")}"
+
+
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.foo.arn}"
+    container_name   = "foo"
+    container_port   = 4000
+  }
+
+  network_configuration {
+    subnets = "${var.subnets}"
+    security_groups = ["${aws_security_group.lb_sg.id}"]
+  }
+}
+
+data "aws_ecs_task_definition" "web" {
+  task_definition = "${aws_ecs_task_definition.web.family}"
+}
+
+resource "aws_ecs_task_definition" "web" {
+  cpu = 512
+  memory = 1024
+  family                = "web"
+  container_definitions = "${file("task-definitions/cd.json")}"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn = "${aws_iam_role.fargate_role.arn}"
+}
+
+resource "aws_lb_target_group" "foo" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+  target_type = "ip"
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.lb_sg.id}"]
+  subnets            = "${var.subnets}"
+
+  enable_deletion_protection = true
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${var.vpc_id}"
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "fargate_role" {
+  name = "${var.appname}-fargate"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "fargate_role_policy" {
+  name = "${var.appname}-fargate_policy"
+  role = "${aws_iam_role.fargate_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:DescribeParameters"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:GetParameters"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lb_listener" "foo" {
+  load_balancer_arn = "${aws_lb.test.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.foo.arn}"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "foo" {
+  name = "/ecs/foo"
+}
+
+resource "random_password" "rds_password" {
+  length = 16
+  special = true
+  override_special = "!#$%&*()-_=+[]{}<>"
+}
+
+resource "aws_ssm_parameter" "rds_password" {
+  name  = "foo_password"
+  type  = "String"
+  value = "${random_password.rds_password.result}"
+}
+
+resource "aws_db_instance" "foo_db" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  instance_class       = "db.t2.micro"
+  name                 = "foo_db"
+  username             = "postgres"
+  password             = "${random_password.rds_password.result}"
+}
+
+resource "aws_ssm_parameter" "rds_ecto_url" {
+  name  = "foo_database_url"
+  type  = "String"
+  value = "ecto://postgres:${random_password.rds_password.result}@${aws_db_instance.foo_db.endpoint}/${var.appname}"
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "region" {}
 variable "appname" {}
 variable "githubRepository" {}
 variable "github_oauth_token" {}
+variable "vpc_id" {}
+variable "subnets" {
+  type    = "list"
+  default = ["fake-subnet"]
+}
